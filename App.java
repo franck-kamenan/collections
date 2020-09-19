af2564cd8778f9f352cb6c41f533d5cda9ae583a
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-19 17:04:04 +0200

    Collection ArrayList addAll asList remove

---
+++ App.java
@@ -0,0 +1,23 @@
+package be.intecbrussel.collections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Integer[] intArray = new Integer[]{2, 5, 8, 11, 6, 10, 8};
+
+        Collection<Integer> intCollection = new ArrayList<>();
+
+        intCollection.addAll(Arrays.asList(intArray));
+        System.out.println(intCollection);
+        intCollection.add(8);
+        intCollection.add(100);
+        System.out.println(intCollection);
+        intCollection.remove(8);
+        System.out.println(intCollection);
+    }
+}
