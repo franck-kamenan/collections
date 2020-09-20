5c85f8fbc708c35dd163febbdf41a0c4f612d8dc
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-20 09:22:13 +0200

    forEach Methode Referentie

--- App.java
+++ App.java
@@ -25,6 +25,6 @@ public class App {
         list.add(11);
         intCollection.removeAll(list);
         System.out.println(intCollection);
-        intCollection.forEach(e -> System.out.println(e));
+        intCollection.forEach(System.out::println);
     }
 }
